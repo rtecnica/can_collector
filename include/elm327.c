6636ab9b865f835ef1c08777aa65a9203356dea0
rTecnica <revisiontecnica@gmail.com>
2018-05-31 19:43:58 -0400

    Enlace funcional. Falta prueba en terreno
    Revertido el orden de conexión
    
    PC --BT--> ESP32 --USB--> ELM327
    
    Mensajes en el LOG desactivados

--- include/elm327.c
+++
@@ -1,7 +0,0 @@
-//
-// Created by Ignacio Maldonado Aylwin on 5/29/18.
-//
-
-#include "elm327.h"
-
-//TODO write ELM327 messaging interface
--- main/can_collector.c
+++ main/can_collector.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <esp_spp_api.h>
 
 #include "nvs.h"
 #include "nvs_flash.h"
@@ -21,15 +20,12 @@
 #include "esp_bt_device.h"
 #include "esp_spp_api.h"
 
-#include "../include/elm327.h"
+#include "../components/elm327.h"
 
 #include "driver/gpio.h"
 
 #include "time.h"
 #include "sys/time.h"
-
-#include "driver/uart.h"
-#include "soc/uart_struct.h"
 
 #define APP_TAG "CAN_DATA_COLLECTOR"
 #define SPP_SERVER_NAME "CAN_DATA_COLLECTOR"
@@ -37,33 +33,12 @@
 
 #define BLINK_GPIO GPIO_NUM_12
 
-#define TXD_PIN GPIO_NUM_1
-#define RXD_PIN GPIO_NUM_3
-
 
 static const esp_spp_mode_t esp_spp_mode = ESP_SPP_MODE_CB;
 static const esp_spp_sec_t sec_mask = ESP_SPP_SEC_NONE;
 static const esp_spp_role_t role_slave = ESP_SPP_ROLE_SLAVE;
 
-
-static const int RX_BUF_SIZE = 1024;
-
 uint32_t bt_handle;
-
-//Función de utilidad para enviar bytestream a través de UART
-int sendData(const char* logName, unsigned char* data, const int len) {
-    union Data{
-        const char *string;
-        unsigned char *raw;
-    }uni;
-
-    uni.raw = data;
-
-    const int txBytes = uart_write_bytes(UART_NUM_0, uni.string, len);
-    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
-    ESP_LOG_BUFFER_HEXDUMP("TX_TASK", data, txBytes, ESP_LOG_INFO);
-    return txBytes;
-}
 
 //Inicialización de pines GPIO para la señalización con LED
 void blink_init() {
@@ -86,43 +61,9 @@ void blink_twice(){
     gpio_set_level(BLINK_GPIO, 0);
 }
 
-// Inicializa el módulo UART #0 que está conectalo a la interfase USB-UART
-void uart_init() {
-    const uart_config_t uart_config = {
-            .baud_rate = 115200,
-            .data_bits = UART_DATA_8_BITS,
-            .parity = UART_PARITY_DISABLE,
-            .stop_bits = UART_STOP_BITS_1,
-            .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    uart_param_config(UART_NUM_0, &uart_config);
-    uart_set_pin(UART_NUM_0, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-    uart_driver_install(UART_NUM_0, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
-}
-
-//Proceso de monitoreo de interfase UART
-static void uart_rx_task() {
-    esp_log_level_set("RX_TASK", ESP_LOG_NONE);
-    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
-
-    while (1) {
-        const int rxBytes = uart_read_bytes(UART_NUM_0, data, RX_BUF_SIZE, 1000 / portTICK_RATE_MS);
-        if (rxBytes > 0) {
-
-            data[rxBytes] = 0;
-            ESP_LOGI("RX_TASK", "Read %d bytes: '%s'", rxBytes, data);
-            ESP_LOG_BUFFER_HEXDUMP("RX_TASK", data, rxBytes, ESP_LOG_INFO);
-
-            esp_spp_write(bt_handle,rxBytes,data);
-            blink_twice();
-        }
-    }
-    free(data);
-}
-
 //Handler de mensaje entrante por BT
 void bt_data_rcv_handler(esp_spp_cb_param_t *param) {
-    sendData("TX_TASK", param->data_ind.data, param->data_ind.len);
+    elm327_sendData("TX_TASK", param->data_ind.data, param->data_ind.len);
     //esp_log_buffer_hex("",param->data_ind.data,param->data_ind.len);
     blink_twice();
 }
@@ -178,8 +119,6 @@ void app_main() {
 
     blink_init();
 
-    uart_init();
-
     esp_log_level_set("*",ESP_LOG_NONE);
 
     esp_err_t ret = nvs_flash_init();
@@ -221,5 +160,6 @@ void app_main() {
         return;
     }
 
-    xTaskCreate(uart_rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES, NULL);
+    elm327_init(&bt_handle);
+
 }
